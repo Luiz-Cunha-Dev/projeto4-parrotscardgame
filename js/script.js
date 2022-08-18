35d4084115ed03f76702b4a1929ca9e263cd3653
Luiz Miguel <Luiz-Cunha-Dev>
2022-08-18 17:00:41 -0300

    qtd de cartas funcionando

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 
 let qtdCartas;
+const qtdMaxCartas = 14;
 
 function inicial (){
 qtdCartas = prompt("Escolha o número de cartas: (número par entre 4-14)");
@@ -20,8 +21,20 @@ else if(qtdCartas%2 !== 0){
 
 inicial ();
 
+let cartasAmenos = qtdMaxCartas - qtdCartas
+
+function cartasTotal(){
+    
+    for(let i = 0; i < cartasAmenos; i++){
+        const cartas = document.querySelectorAll("#card");
+        cartas[i].classList.add('hidden')
+    }
+}
+
+cartasTotal();
 
 
 function flipCard(card){
     card.classList.add('flip')
 }
+
